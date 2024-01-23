2fbc6b5ca5325bfb98cfb230896e82d3e3fcd652
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2024-01-23 10:23:12 -0600

    first commit

---
+++ notes.md
@@ -0,0 +1,6 @@
+npm init --y
+
+npm i express express-handlebars morgan mysql2
+npm i nodemon -D
+
+node ./src/index.js
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+// --- Initialization ---
+
+// --- Settings ---
+
+// --- Middlewares ---
+
+// --- Routes ---
+
+// --- Public Files ---
+
+// --- Run Server ---
