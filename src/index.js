e15171efe985925198bb3c106bffe0929adf56df
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2024-01-23 10:48:57 -0600

    correcion de errores

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // --- Libraries ---
-import { express } from 'express';
-import { morgan } from 'morgan';
+import express from 'express';
+import morgan from 'morgan';
 import { engine } from 'express-handlebars';
-import {join, dirname} from 'path';
+import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 // --- Initialization ---
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 // --- Settings ---
 app.set('port', process.env.PORT || 3000);
-app.set('views', join(__dirname, 'views'));
+app.set('views', join(currentDir, 'views'));
 app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: join(app.get('views'), 'layouts'),
@@ -22,16 +22,16 @@ app.set('view engine', '.hbs');
 
 // --- Middlewares ---
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // --- Routes ---
-app.get('/', (req, res)=> {
-    res.json({"message": "Hello World"});
+app.get('/', (req, res) => {
+    res.json({ "message": "Hello World" });
 });
 
 // --- Public Files ---
 
 // --- Run Server ---
-app.listen(app.get('port'), ()=> 
+app.listen(app.get('port'), () =>
     console.log('Server listening on port', app.get('port')));
