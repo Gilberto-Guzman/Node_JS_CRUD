97e3352ec82d9387e652e0a956fce27deb70dbe6
Gilberto-Guzman <josegilbertoguzmangutierrez@gmail.com>
2024-01-24 10:06:47 -0600

    crear y listar datos

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -7,6 +7,19 @@ router.get('/add', (req, res)=>{
     res.render('personas/add');
 });
 
+router.post('/add', async(req, res)=>{
+    try{
+        const {name, lastname, age} = req.body;
+        const newPersona = {name, lastname, age}
+
+        await pool.query('INSERT INTO personas SET ?', [newPersona]);
+        res.redirect('/list');
+    }
+    catch{
+        res.status(500).json({message:err.message});       
+    }
+});
+
 router.get('/list', async(req, res)=>{
     try{
         const [result] = await pool.query('SELECT * FROM personas');
